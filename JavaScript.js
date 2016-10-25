9a6811d9f071774a56e728202d166173a63805ce
h181185 <jacobaas@hotmail.com>
2016-10-25 15:43:36 +0200

    V.1.0.3

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,4 @@
-﻿
+
 /* Lukk menyen ved å trykke en annen plass på skjermen, som ikke inneholder meny elementer */ 
 if (screen && screen.width > 641) {
     document.write($(document).click(function (event) {
@@ -61,3 +61,34 @@ $('.inner').click(function (e) {
 })
 
 
+function loadDoc() {
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var xml = xhttp.responseXML;
+            writeData(xml);
+        }
+    }
+
+    xhttp.open("GET", "Prosjektxml.xml", true);
+    xhttp.send();
+}
+
+
+function writeData(xml) {
+
+    if (xml) { //xml != null
+        var elements = xml.getElementsByTagName("todo");
+        var display = document.getElementById("display");
+        var table = "<table><tr><th> Nøkkelen til godt samarbeid</th></tr>";
+        for (i = 0; i < elements.length; i++) {
+            table +=
+                "<tr><td>" + elements[i].getElementsByTagName("Linker")[0].textContent + "</td></tr>";
+        }
+        table += "</table>";
+        display.innerHTML = table;
+
+    }
+}
